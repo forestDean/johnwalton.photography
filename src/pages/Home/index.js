2dfe69167f6b5dad6ebb7405b293708f88bd9581
forestDean <113769986+forestDean@users.noreply.github.com>
2023-07-01 14:14:39 +0100

    setup bootstrap

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+<div>        
+<h1 className="text-center mt-5 text-muted">John Walton Photography</h1>
+</div>
+    );
+  }
+
+export default Home
+
